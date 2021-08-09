ce449b633c14353890fae175dc3f4c0402639de2
Konstantin L <kosto55@mail.ru>
2021-08-09 18:24:09 +0300

    reset mail

--- app/models.py
+++ app/models.py
@@ -1,12 +1,12 @@
-# -*- coding: utf-8 -*-
+import jwt
 from datetime import datetime
-from typing import Union
+from time import time
 from flask_login import UserMixin
 from hashlib import md5
 from sqlalchemy.orm import backref
 from werkzeug.security import generate_password_hash, check_password_hash
 
-from app import db, login
+from app import app, db, login
 
 
 @login.user_loader
@@ -61,6 +61,20 @@ class User(UserMixin, db.Model):
                 followers.c.follower_id == self.id)
         own = Post.query.filter_by(user_id=self.id)
         return followed.union(own).order_by(Post.timestamp.desc())
+    
+    def get_reset_password_token(self, expires_in=600):
+        return jwt.encode(
+            {'reset_password': self.id, 'exp': time() + expires_in},
+            app.config['SECRET_KEY'], algorithm='HS256').decode('utf-8')
+    
+    @staticmethod
+    def verify_reset_password_token(token):
+        try:
+            id = jwt.decode(token, app.config['SECRET_KEY'],
+            algorithms=['HS256'])['reset_password']
+        except:
+            return
+        return User.query.get(id)
     
     def __repr__(self):
         return f'User {self.username}'
