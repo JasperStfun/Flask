2fa00fa1ef97ee7de4d83a646858cb16e51f9382
Konstantin L <kosto55@mail.ru>
2021-08-14 15:01:08 +0300

    translate

--- app/__init__.py
+++ app/__init__.py
@@ -58,8 +58,7 @@ if not app.debug:
 
 @babel.localeselector
 def get_locale():
-    #return request.accept_languages.best_match(app.config['LANGUAGES'])
-    return 'en'
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
 
 
 from app import routes, models, errors
